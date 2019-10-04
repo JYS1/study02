c9aeaf38700fe97424993aa88dcc20d2e6c3664d
JYS1 <46517021+JYS1@users.noreply.github.com>
2019-10-04 17:38:30 +0900

    HTML - 05
    기억이 안난다 허허허

---
+++ Test/index.js
@@ -0,0 +1,27 @@
+
+function sub() {
+    var name = document.getElementById("name").value;
+    var sub1 = document.getElementById("sub1").value;
+    var sub2 = document.getElementById("sub2").value;
+    var sub3 = document.getElementById("sub3").value;
+
+    var sum;
+    var avg;
+    var grade;
+    sum = parseInt(sub1) + parseInt(sub2) + parseInt(sub3);
+    document.getElementById("sum").value;
+    avg = sum/3;
+    document.getElementById("avg").value;
+
+    if( avg > 90) {
+        grade = "Excellent";
+    } else if (avg < 50) {
+        grade = "Fail";
+    } else {
+        grade = "Normal";
+    }
+    document.getElementById("grade").value;
+}
+
+var jbAry = new
+document.write( jbAry);
